c775224da07af51739f17b71ba3aa8c9e589ef9b
Tucker Brady <tuck.al.brady@gmail.com>
2020-04-02 21:37:47 -0600

    * Index.cshtml: added a renderCell function
    * chat.js: cleaned up the console.logs

--- ChatApp/wwwroot/js/chat.js
+++ ChatApp/wwwroot/js/chat.js
@@ -4,27 +4,21 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build(); //local setting
 
 connection.start().then(function () {
-    console.log("Start");
     document.getElementById("grid").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
 connection.on("ReceiveMessage", function (color, playerGameBoard) {
-    console.log("receiveMessage from the connection in chat.js");
-    console.log(color);
-    console.log(playerGameBoard);
     gridArray = playerGameBoard;
     reRender();
 });
 
 connection.on("ReceiveMath", function () {
-    console.log("ReceiveMath from the connection in chat.js");
     gameMath();
 });
 
 document.getElementById("grid").addEventListener("click", function (event) {
-    console.log("Click from the event listener in chat.js");
 
     connection.invoke("SendMessage", color, gridArray).catch(function (err) {
         return console.error(err);
@@ -32,11 +26,8 @@ document.getElementById("grid").addEventListener("click", function (event) {
     event.preventDefault();
 });
 
-document.getElementById("gamePlay").addEventListener("click", function (event2)
-{
-    console.log("Click from next button");
-    connection.invoke("SendMath").catch(function (err)
-    {
+document.getElementById("gamePlay").addEventListener("click", function (event2) {
+    connection.invoke("SendMath").catch(function (err) {
         return console.error(err);
     });
     event2.preventDefault();
